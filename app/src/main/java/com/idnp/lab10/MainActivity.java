c5e2e5e3ab7c5314957dade754d430a5a5eec346
gelber12345 <gmartinezpas@unsa.edu.pe>
2021-12-02 17:24:42 -0500

    Initial commit

---
+++ app/src/main/java/com/idnp/lab10/MainActivity.java
@@ -0,0 +1,77 @@
+package com.idnp.lab10;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    double latitud,longitud;
+    TextView txtLatitud,txtLongitud;
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtLatitud = findViewById(R.id.txtValorLatitud);
+        txtLongitud= findViewById(R.id.txtValorLongitud);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
+        }
+        Log.d("Permiso","ENtre a permisos");
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitud = location.getLatitude();
+        longitud = location.getLongitude();
+
+        txtLongitud.setText("" + longitud);
+        txtLatitud.setText(""+latitud);
+
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+}
